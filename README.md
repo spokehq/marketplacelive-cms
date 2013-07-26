2d83fd68a523d826443209f2858c7f003593d240
Jared Rosenberg <jrosenberg@aktion.com>
2013-07-26 10:00:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+
+#####The setup includes:
+* box:       precise64 (http://files.vagrantup.com/precise64.box)* webserver: apache
+* database:  mysql
+* PHP:       PHP 5.3
+
+####Next steps:
+**If you have not installed Vagrant yet, go to http://downloads.vagrantup.com/**
+
+##Starting a new Project
+Fork or clone this Repository
+
+Go into this directory and
+
+$ vagrant up
+
+Afterwards you may access your box  from the same directory with
+
+$ vagrant ssh
+
+More documentation:
+* Vagrant >= 1.1: http://docs.vagrantup.com/v2/vagrantfile/ssh_settings.html
+* Vagrant <  1.1: http://docs-v1.vagrantup.com/v1/docs/getting-started/ssh.html
+
+####Please change the following **project**: files:
+**Vagrantfile**
+
+      # in case you have a couple VMs running at the same time
+
+      config.vm.network :private_network, ip: "{SOMETHING.UNIQUE.GOES.HERE}"
+
+      ...
+
+      v.customize ["modifyvm", :id, "--name", "{SOMETHING}.{DEV}"]
+
+**manifests/default.pp**
+
+    Config Vars Section
+
+        $dev_site_name = '{SOMETHING}.{DEV}'
+
+        ...
+        # these are not for root MySQL but a MySQL user
+        $db_name    = '{DATABASE_NAME}'
+        $db_user    = '{DATABASE_USER}'
+        $db_pass    = '{DATABASE_PASSWORD}'
+        
+####Lastly
+
+You may need to edit your **local**: hosts file and add a hostname/IP mapping to be able to accesss the site at it's $dev_site_name.
+
+[Wiki Page](https://bitbucket.org/jaredrhodes/lamp-project-template-ubuntu-12.04-lts/wiki)
---
+++ manifests/default.pp
@@ -0,0 +1,190 @@
+# what to name the site?
+$dev_site_name = ''
+
+# mysql creds, whatever you want them to be
+$db_name    = ''
+$db_user    = ''
+$db_pass    = ''
+
+
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    
+file { '/home/vagrant/.bash_aliases':
+  ensure => 'present',
+  source => 'puppet:///modules/puphpet/dot/.bash_aliases',
+}
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core',
+    'htop',
+  ]:
+  ensure  => 'installed',
+}
+
+service { 'ufw':
+  ensure => 'stopped',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { $dev_site_name:
+  server_name   => $dev_site_name,
+  serveraliases => [
+],
+  docroot       => '/var/www/site',
+  port          => '80',
+  env_variables => [
+],
+  priority      => '1',
+}
+
+class { 'php':
+  service       => 'apache',
+  module_prefix => '',
+  require       => Package['apache'],
+}
+
+php::module { 'php5-mysql': }
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+
+
+php::pecl::module { 'xhprof':
+  use_package     => false,
+  preferred_state => 'beta',
+}
+
+apache::vhost { 'xhprof':
+  server_name => 'xhprof',
+  docroot     => '/var/www/xhprof/xhprof_html',
+  port        => 80,
+  priority    => '1',
+  require     => Php::Pecl::Module['xhprof']
+}
+
+
+class { 'xdebug':
+  service => 'apache',
+}
+
+class { 'composer':
+  require => Package['php5', 'curl'],
+}
+
+puphpet::ini { 'xdebug':
+  value   => [
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 0',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_xdebug.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "America/Detroit"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = -1'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+
+class { 'mysql::server':
+  config_hash   => { 'root_password' => 'akt@1687' }
+}
+
+mysql::db { $db_name:
+  grant    => [
+    'ALL'
+  ],
+  user     => $db_user,
+  password => $db_pass,
+  host     => 'localhost',
+  charset  => 'utf8',
+  require  => Class['mysql::server'],
+}
+
+exec { "import-schema":
+      command => "mysql -u ${db_user} --password=${db_pass} ${db_name} < /var/www/db/schema.sql",
+      onlyif  => "[ -f /var/www/db/schema.sql ]",
+      require  => Class['phpmyadmin'],
+}
+
+exec { "import-data":
+      command => "mysql -u ${db_user} --password=${db_pass} ${db_name} < /var/www/db/data.sql",
+      onlyif  => "[ -f /var/www/db/data.sql ]",
+      require => Exec['import-schema'],
+}
+
+class { 'phpmyadmin':
+  require => [Class['mysql::server'], Class['mysql::config'], Class['php']],
+}
+
+apache::vhost { 'phpmyadmin':
+  server_name => 'phpmyadmin',
+  docroot     => '/usr/share/phpmyadmin',
+  port        => 80,
+  priority    => '10',
+  require     => Class['phpmyadmin'],
+}
+
+file { 'git-hooks':
+  path    => '/var/www/.git/hooks/pre-commit',
+  ensure  => present,
+  mode    => 744,
+  content => "#! /bin/sh
+  # export schema
+  mysqldump -u ${db_user} --password=${db_pass} --no-data --no-create-db ${db_name} > db/schema.sql
+  #export data
+  mysqldump -u ${db_user} --password=${db_pass} --no-create-info --skip-extended-insert ${db_name} > db/data.sql
+  git add db/*",
+  require => Package['git-core'],
+}
