e9f9ce6281062f7908b9ba3e2906de4586951957
Greg <shrinkfolio@gmail.com>
2016-05-19 21:17:11 -0400

    Rebuild Footer layout Theme Twitget Setup social icons

--- site/wp-content/themes/marketplacelive-theme/templates/footer.php
+++ site/wp-content/themes/marketplacelive-theme/templates/footer.php
@@ -1,11 +1,13 @@
 <footer class="content-info" role="contentinfo">
-  <div class="">
+
     <div class="row">
         <div class="col-sm-6">
             <div class="adjustment">
-                <h4>A COMMUNITY BUILDING FOR DIGITAL SUCCESS</h4>
+                <h3>A COMMUNITY BUILDING FOR DIGITAL SUCCESS</h3>
             </div>
         </div>
+
+
 
         <div class="col-sm-6">
             <div class="vignette">
@@ -29,42 +31,34 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-4">
-            <h4>IN THE NEWS</h4>
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-sm-4">
+                <?php dynamic_sidebar('sidebar-footer-1'); ?>
+            </div>
 
-            <img class="alignleft" src="{$profile_image}">
-            <a href="https://www.twitter.com/{$user_twitter_name}">@{$user_twitter_name}</a>
-            <br />
-            {$user_description}
-            <ul class="pages">
-                {$tweets_start}
-                <li>{$tweet_text}<br />{$tweet_time}</li>
-                {$tweets_end}
-            </ul>
-            <?php dynamic_sidebar('sidebar-footer-1'); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-3">
-            <?php dynamic_sidebar('sidebar-footer-2'); ?>
-        </div>
-        <div class="col-sm-3">
-            <a href="https://marketplacelive2016.eventbrite.com/" class="btn btn-standard" type="button" target="_blank" >REGISTER</a>
-        </div>
-        <div class="col-sm-offset-6">
-            <div class="hosting"><span class="proudly-hosted">PROUDLY&nbsp;HOSTED&nbsp;BY</span>
-                <img src="<?php echo get_template_directory_uri(); ?>/dist/images/footer-telx-logo.png" alt="Digital Realty | TELX" class="digital-realty"></div>
+            <div class="col-sm-3">
+                <?php dynamic_sidebar('sidebar-footer-2'); ?>
+            </div>
+
+            <div class="col-sm-3">
+                <a href="https://marketplacelive2016.eventbrite.com/" class="btn btn-standard" type="button" target="_blank" >REGISTER</a>
+            </div>
+
+            <div class="col-sm-offset-6">
+                <div class="hosting"><span class="proudly-hosted">PROUDLY&nbsp;HOSTED&nbsp;BY</span>
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/footer-telx-logo.png"
+                         alt="Digital Realty | TELX" class="digital-realty">
+                </div>
             </div>
         </div>
-    </div>
 
-
-    <div class="row">
-      <?php dynamic_sidebar('sidebar-footer-full-bottom'); ?>
-      <div class="col-sm-12">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-      </div>
+        <div class="row">
+          <?php dynamic_sidebar('sidebar-footer-full-bottom'); ?>
+          <div class="col-sm-12">
+            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+          </div>
+        </div>
     </div>
-  </div>
 </footer>
