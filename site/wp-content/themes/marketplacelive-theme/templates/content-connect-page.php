e30049d8b59245646094e24df77787bfe3ddfa52
Greg <shrinkfolio@gmail.com>
2016-05-26 16:32:58 -0400

    Create Connect & Venue pages -Styling updates

--- site/wp-content/themes/marketplacelive-theme/templates/content-connect-page.php
+++ site/wp-content/themes/marketplacelive-theme/templates/content-connect-page.php
@@ -2,20 +2,93 @@
 <!-- .main removed from base template  -->
 
 <main class="main">
+    <div class="container">
+        <div class="main-content-row">
 
-
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-8">
-                            <div class="message-wrapper">
-                                <?php the_field('come_back_soon'); ?>
-                            </div>
-                        </div>
-                    </div>
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-10">
+                    <?php the_field('connect_page_heading'); ?>
                 </div>
+            </div>
+            <div class="row">
 
 
+
+                <div class="col-sm-offset-1 col-sm-10">
+                    <?php if( have_rows('add_connection') ): ?>
+
+
+                        <?php while( have_rows('add_connection') ): the_row();
+
+                            // Telx Member vars
+
+                            $t_role = get_sub_field('functional_role');
+                            $t_member = get_sub_field('member_name');
+                            $t_title = get_sub_field('member_title');
+                            $t_phone = get_sub_field('member_phone');
+                            $t_email = get_sub_field('member_email');
+
+
+
+                            ?>
+
+
+                            <div class="member-wrapper">
+                                <div class="col-sm-6 ">
+                                   <div class="role">
+                                       <?php echo $t_role ?>
+                                   </div>
+                                   <div class="member">
+                                       <?php echo $t_member ?>
+                                   </div>
+                                   <div class="title">
+                                       <?php echo $t_title ?>
+                                   </div>
+                                   <div class="phone">
+                                       <?php echo $t_phone ?>
+                                   </div>
+                                   <div class="email">
+                                       <?php echo $t_email ?>
+                                   </div>
+                               </div>
+                            </div>
+
+
+
+                        <?php endwhile; ?>
+
+                    <?php endif; ?>
+                 </div>
+            </div>
+        </div>
+
+        <div class="main-content-row">
+            <div class="row">
+                <div class="social-wrapper">
+                    <div class="col-sm-offset-2 col-sm-8 col-xs-12">
+                        <ul class="social">
+                            <li class="item"><a href="https://vimeo.com/telx"
+                                                target="_blank"><img class="icon" src="<?= get_template_directory_uri(); ?>/dist/images/soc-vimeo.svg"
+                                                                     target="_blank" alt=""></a></li>
+                            <li class="item"><a href="https://plus.google.com/+TelxGroup"
+                                                target="_blank"><img class="icon" src="<?= get_template_directory_uri(); ?>/dist/images/googleplus.svg"
+                                                                     target="_blank" alt=""></a></li>
+                            <li class="item"><a href="https://twitter.com/TelxMPLIVE"
+                                                target="_blank"><img class="icon" src="<?= get_template_directory_uri(); ?>/dist/images/soc-twitter.svg"
+                                                                     target="_blank" alt=""></a></li>
+                            <li class="item"><a href="http://www.linkedin.com/company/telx"
+                                                target="_blank"><img class="icon" src="<?= get_template_directory_uri(); ?>/dist/images/soc-linkedin.svg"
+                                                                     target="_blank" alt=""></a></li>
+                            <li class="item"><a href="https://www.facebook.com/MarketplaceLive/"
+                                                target="_blank"><img class="icon" src="<?= get_template_directory_uri(); ?>/dist/images/soc-fb.svg"
+                                                                     target="_blank" alt=""></a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 
--- site/wp-content/themes/marketplacelive-theme/templates/content-venue-page.php
+++ site/wp-content/themes/marketplacelive-theme/templates/content-venue-page.php
@@ -1,21 +1,115 @@
-
 <!-- .main removed from base template  -->
 
 <main class="main">
 
 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-8">
-                            <div class="message-wrapper">
-                                <?php the_field('come_back_soon'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="main-content-row">
+                <?php the_field('venue_introduction'); ?>
+            </div>
+        </div>
+    </div>
+
+    <?php if( have_rows('venue_row_layout') ): ?>
+
+
+        <?php while( have_rows('venue_row_layout') ): the_row();
+
+            // vars
+
+            $heading = get_sub_field('venue_heading');
+            $desc = get_sub_field('venue_description');
+            $toggle_cta = get_sub_field('add_venue_cta');             // display button?
+            $cta_button = get_sub_field('venue_cta');
+            $cta_link = get_sub_field('venue_cta_link');
+            $cta_tab = get_sub_field('venue_new_tab');
+            $which_image = get_sub_field('venue_image_spot');         // left or right?
+            $image_left = get_sub_field('venue_image_on_left');
+            $image_right = get_sub_field('venue_image_on_right');
+
+
+
+            ?>
+
+
+
+
+            <?php if ( $which_image === "Left" ) {
+
+                // image on left
+
+                ?>
+                <div class="row">
+                    <div class="main-content-row left-img">
+                        <div class="right-text">
+
+                            <div class="col-sm-4">
+                                <img src="<?php echo $image_left['url']; ?>" alt="<?php echo $image_left['alt']; ?>"
+                                     class="image-left parallax img-responsive" />
                             </div>
+                            <div class="col-sm-4 col-sm-offset-1">
+                                <div class="text-wrapper">
+                                    <div class="heading">
+                                        <h2><?php echo $heading; ?></h2>
+                                    </div>
+
+                                    <div class="description">
+                                        <?php echo $desc; ?>
+                                    </div>
+
+                                    <div class="cta">
+                                        <a class="btn btn-standard clearfix" href="<?php echo $cta_link; ?>" ><?php echo $cta_button; ?></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            <?php } elseif ( $which_image === "Right" ) {
+
+                // image on right
+
+                ?>
+                <div class="row">
+                    <div class="main-content-row right-img">
+                        <div class="left-text">
+                            <div class="col-sm-4 col-sm-offset-3">
+                                <div class="text-wrapper">
+                                    <div class="heading">
+                                        <h2><?php echo $heading; ?></h2>
+                                    </div>
+
+                                    <div class="description">
+                                        <?php echo $desc; ?>
+                                    </div>
+
+                                    <div class="cta">
+                                        <a class="btn btn-standard clearfix" href="<?php echo $cta_link; ?>" ><?php echo $cta_button; ?></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--  stack this image on top when in mobile view or remove image -->
+                            <div class="col-sm-4 col-sm-offset-1">
+                                <img src="<?php echo $image_right['url']; ?>" alt="<?php echo $image_right['alt']; ?>" class="image-right img-responsive" />
+                            </div>
+
                         </div>
                     </div>
                 </div>
 
+            <?php } else {
+                // do nothing
 
-</main>
+            } ?>
+
+
+        <?php endwhile; ?>
+
+    <?php endif; ?>
+
+
 
 
