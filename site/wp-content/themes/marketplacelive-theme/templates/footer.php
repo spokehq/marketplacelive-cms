b02a891413be5c932a61e7194cea8c36d64f70f0
Greg <shrinkfolio@gmail.com>
2016-05-31 11:35:36 -0400

    Add Stat Counter on site

--- site/wp-content/themes/marketplacelive-theme/templates/footer.php
+++ site/wp-content/themes/marketplacelive-theme/templates/footer.php
@@ -82,3 +82,21 @@
         </div>
     </div>
 </footer>
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+    var sc_project=10991407;
+    var sc_invisible=1;
+    var sc_security="be3e9043";
+    var sc_https=1;
+    var scJsHost = (("https:" == document.location.protocol) ?
+        "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" +
+        scJsHost+
+        "statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="hit counter"
+                                      href="http://statcounter.com/" target="_blank"><img
+                class="statcounter"
+                src="//c.statcounter.com/10991407/0/be3e9043/1/" alt="hit
+counter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
