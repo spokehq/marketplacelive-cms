219474616345d49618a685d24da064b03ceef023
Greg <greg@spokehq.com>
2016-06-15 14:45:32 -0400

    database snapshot -- Update twitget plugin data for new handle

--- db/schema.sql
+++ db/schema.sql
@@ -105,7 +105,7 @@ CREATE TABLE `si121_options` (
   `autoload` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'yes',
   PRIMARY KEY (`option_id`),
   UNIQUE KEY `option_name` (`option_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=621 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=635 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -289,4 +289,4 @@ CREATE TABLE `si121_users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-15  0:34:12
+-- Dump completed on 2016-06-15 14:45:32
